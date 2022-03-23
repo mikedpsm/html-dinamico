81598eb1210dadc47e24fd0304f197fb19099b8b
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2022-03-23 18:07:25 -0300

    Dados do usuario adicionados

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ fetch("http://jsonplaceholder.typicode.com/users").then((resposta) => {
       const name = document.createElement("h1");
       const username = document.createElement("p");
       const email = document.createElement("p");
+      const website = document.createElement("p");
 
       name.textContent = user.name;
       username.textContent = user.username;
       email.textContent = user.email;
+      website.textContent = user.website;
 
-      div.append(name, username, email);
+      div.append(name, username, email, website);
 
       body.append(div);
     });
