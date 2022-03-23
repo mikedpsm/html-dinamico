7962ace5c76497f931d20f0ef2ab4f61bbd9948c
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2022-03-23 18:48:35 -0300

    Remoção de item da lista

--- dinamico-02/script.js
+++ dinamico-02/script.js
@@ -5,8 +5,12 @@ input.addEventListener("keydown", (event) => {
   if (event.key !== "Enter" || input.value === "") return;
 
   const task = document.createElement("li");
-
   task.textContent = input.value;
+
+  task.addEventListener("click", () => {
+    task.remove();
+  });
+
   tasks.append(task);
 
   input.value = "";
