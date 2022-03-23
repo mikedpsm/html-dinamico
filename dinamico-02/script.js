fa25f2b0618d05ff803cd1c3cc25e0b2b0f704a9
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2022-03-23 18:24:19 -0300

    Codigo defensivo + criaçao de itens

--- dinamico-02/script.js
+++ dinamico-02/script.js
@@ -1,7 +1,13 @@
 const input = document.querySelector("input");
+const tasks = document.querySelector("ul");
 
 input.addEventListener("keydown", (event) => {
-  if (event.key !== "Enter") return;
+  if (event.key !== "Enter" || input.value === "") return;
+
+  const task = document.createElement("li");
+
+  task.textContent = input.value;
+  tasks.append(task);
 
   input.value = "";
 });
